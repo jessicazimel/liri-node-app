5510e42805de6de64278c3379b6b00064fe1950e
jessicazimel <34245792+jessicazimel@users.noreply.github.com>
2018-04-14 13:00:50 -0400

    Add files via upload

--- liri-node-app/liri.js
+++ liri-node-app/liri.js
@@ -1,1 +1,34 @@
 require("dotenv").config();
+
+var keys = require("./keys.js");
+console.log(keys);
+
+
+var Twitter = require('twitter');
+ 
+var client = new Twitter(keys.twitter);
+ 
+var params = {screen_name: 'jessicazimel'};
+client.get('statuses/user_timeline', params, function(error, tweets, response) {
+  if (!error) {
+    console.log(tweets);
+  }
+  
+});
+
+var Spotify = require('node-spotify-api');
+var inquirer = require('inquirer');
+require("dotenv").config();
+
+var spotify = new Spotify({
+  id: ('jesszimel'),
+});
+ 
+spotify
+  .request('https://api.spotify.com/v1/tracks/7yCPwWs66K8Ba5lFuU2bcx')
+  .then(function(data) {
+    console.log(data); 
+  })
+  .catch(function(err) {
+    console.error('Error occurred: ' + err);
+  })
